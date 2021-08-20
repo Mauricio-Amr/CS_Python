a23e816aaa3342539cfe20858876ef863ab84ee6
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2021-08-19 23:08:23 -0300

    Inicio
    aulas iniciais

---
+++ Python_Basic/Aula1/aula_1.py
@@ -0,0 +1,24 @@
+# Isso  é um comentario
+
+print('Linha 1')
+print('Linha 2')
+# Falando sobre do que se trata essa linha
+print('Linha 3')
+# uma linha
+# outa linha
+print('Linha 4')  # Isso é um comentario
+print('Linha 5')  # Outro comentario
+
+# comentario com
+"""
+print('Linha 6')
+print('Linha 6')
+print('Linha 6')
+"""
+
+# Comentario com
+'''
+
+Algun texto 
+
+'''
---
+++ Python_Basic/Aula2/aula_2.py
@@ -0,0 +1,8 @@
+print(123456)
+print('Mauricio','Amaral', 'Outra Coisa')
+
+"""
+824.176.070-18
+"""
+print('824','176', '070', sep='.', end='-')
+print('18')
