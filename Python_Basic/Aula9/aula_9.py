e2a5954372afaf01a85ad3d32b942cd48a72a6e1
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2021-08-26 15:10:43 -0300

    Aula 9
    concluida

---
+++ Python_Basic/Aula9/aula_9.py
@@ -0,0 +1,20 @@
+"""
+if , elif, else
+"""
+
+if False :
+    print('Verdadeiro')
+    print ('teste teste2')
+elif True :
+    print('Agora é verdadeiro')
+
+    nome = input('Digite seu nome ')
+    print(f'seu nome é {nome}')
+
+    
+elif False :
+    print('Mais uma veradeira')
+    print(22+22)
+else:
+    print('Não é verdadeiros')
+    print('olá')
