bba7ac612d8ca386648b2d8c4df6c481171efcd6
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2021-09-03 20:07:37 -0300

    desafio contadores concluido

--- Python_Basic/Desafio3/contadores.py
+++ Python_Basic/Desafio3/contadores.py
@@ -1,9 +1,15 @@
-
 contador = 0
 contador_rev = 10
 
-while contador <= 10 :
-    print(f'{contador} : {contador_rev}')
-    contador += 1
-    contador_rev -= 1
+# while contador <= 10 :
+#     print(f'{contador} : {contador_rev}')
+#     contador += 1
+#    contador_rev -= 1
 
+# for valor in range(0,10):
+#     print(f'{contador} : {contador_rev}')
+#     contador += 1
+#     contador_rev -= 1
+
+for i, v in enumerate(range(10, 0, -1)):
+    print(i, v)
