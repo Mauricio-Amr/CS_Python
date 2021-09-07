5348faeaca61357d5c06334a0cf0c17a0ab8ede2
mauricio-amr <mauricio.s.amr@gmail.com>
2021-09-07 17:45:09 -0300

    Aula Python procedural 1 - função 3

--- Python_Procedural/Aula1/aula_1-Funcoes3.py
+++ Python_Procedural/Aula1/aula_1-Funcoes3.py
@@ -3,4 +3,27 @@ Funções (def) em Python - *args **kwargs
 
 """
 
-def func():
+def func(a1, a2, a3, a4, a5 , nome = None , a6 =None):
+    print(a1, a2, a3, a4, a5, nome, a6)
+    return nome, a6
+
+def func2(*args, **kwargs):
+
+  print(args)
+  print(kwargs)
+  idade  = kwargs.get('idade')
+  if idade is not None:
+      print(idade)
+
+
+
+lista = [1,2,3,4,5]
+lista2 = [10,20,30,40,50]
+func2(*lista, lista2 , nome = 'luiz', sobrenome ='Miranda', idade = 30)
+
+
+
+print('################################')
+
+var = func(1,2,3,4,5, nome = 'mauricio', a6 = '1')
+print (var[0], var[1])
