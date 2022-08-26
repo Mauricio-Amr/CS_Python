11d3c3b19aa3331746f5ca17027360a22b50952c
Mauricio-Amr <mauricio.s.amr@gmail.com>
2022-08-25 21:54:04 -0300

    configuração  do vs code no linux

--- Python_Modules/If_name/module.py
+++
@@ -1,8 +0,0 @@
-
-def soma(x: float, y: float) -> float:
-    return x + y
-
-
-if __name__ == '__main__':
-    print(soma(20, 30))
-    print(soma(30, 40))
---
+++ Python_Modulos/Aula 130/module.py
@@ -0,0 +1,14 @@
+from unicodedata import name
+
+
+def soma(x: float, y: float) -> float:
+    return x + y
+
+
+def main() -> None:
+    print(soma(50, 20))
+
+
+"""utilizado  para teste local do  arquivo """
+if __name__ == "__main__":
+    main()
